12136258ba19bc18058908d9539a8a26fab31cb0
walter <buczacka.walter@gmail.com>
2018-01-11 11:54:35 -0200

    add router

---
+++ router.js
@@ -0,0 +1,15 @@
+const auth = require('./controllers/authentication')
+const passportService = require('./services/passport')
+const passport = require('passport')
+
+const requireAuthorize = passport.authenticate('jwt', { session: false })
+const requireSignin = passport.authenticate('local', { session: false })
+
+module.exports = function(app) {
+
+  app.get('/', requireAuthorize, (req, res) => {
+    res.send({message: 'Hello'})
+  })
+  app.post('/signin', requireSignin , auth.signin)
+  app.post('/signup', auth.signup)
+}
